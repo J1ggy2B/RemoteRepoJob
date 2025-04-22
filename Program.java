3fcdb8d4d4aab28ad73b9b88cb158cde59f637bb
J1ggy2B <watsongraeme418@mail.uk>
2025-04-22 12:26:48 +0100

    new version

--- Program.java
+++ Program.java
@@ -1,8 +1,9 @@
 import java.io.*;
 public class Program{
 public static void main(String[] args){
-
-System.out.println("Hello User");
+for(String str : args){
+System.out.println("Hello " + str);
+}
 }
 }
 
